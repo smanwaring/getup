b36c8c41f7b3e56376129f13fbd42049f8048503
Andrew Garcia <runandrew@gmail.com>
2016-12-13 15:50:09 -0500

    fixed the setInterval bug

--- browser/Timer.js
+++ browser/Timer.js
@@ -7,33 +7,34 @@ class Timer extends React.Component {
   }
 
   componentDidMount(){
-    console.log("INTERVAL", this.props.interval)
     let time = +this.props.interval * (5000);
-    
+    console.log("INTERVAL", this.props.interval)
+    console.log("time", time, typeof time)
+
     window.setInterval(function(){
       //notification code goes here
       Notification.requestPermission().then(function(result) {
         console.log(result);
         // const createNotification = () => {
           const title = "Get up!";
-          
+
           const onClickHandler = () => {
             console.log("clicked the notification");
             window.open('https://www.washingtonpost.com/apps/g/page/national/the-health-hazards-of-sitting/750/', '_blank');
           }
-          
+
           const options = {
             body: "Sitting too long can cause oragan damage, such as heart disease :\(\n\nClick here for some stretches you can do at your desk!",
             icon: "http://www.freeiconspng.com/uploads/exercise-icon-1.png"
           }
-          
+
           const notify = new Notification(title, options);
           notify.onclick = onClickHandler;
-          
+
         // }
       });
 
-    },10000);
+  }, time);
   }
 
   render () {
